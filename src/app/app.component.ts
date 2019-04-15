139ec4a473ed45675ca4ce53db4dd85f2ef0e360
Checo Ponce De Leon <checopdleon@gmail.com>
2019-04-15 15:49:34 -0600

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  showHeroes:boolean = false;
+
+  toggleHeroes() {
+  	this.showHeroes = !this.showHeroes;
+  }
+}
